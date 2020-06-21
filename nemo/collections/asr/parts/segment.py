3feac0cdde4e1eb8121251ea0986d418115effeb
jbalam-nv <4916480+jbalam-nv@users.noreply.github.com>
2020-06-21 16:35:16 -0700

    Debug log fix

--- nemo/collections/asr/parts/segment.py
+++ nemo/collections/asr/parts/segment.py
@@ -52,12 +52,11 @@ class AudioSegment(object):
 
     def __str__(self):
         """Return human-readable representation of segment."""
-        return "%s: num_samples=%d, sample_rate=%d, duration=%.2fsec, rms=%.2fdB" % (
+        return "%s: num_samples=%d, sample_rate=%d, duration=%.2fsec" % (
             type(self),
             self.num_samples,
             self.sample_rate,
             self.duration,
-            self.rms_db,
         )
 
     @staticmethod
