4417b2ccbdb4abb1625fb68e84ffe0d04f99ff52
Yang Zhang <yangzhang@nvidia.com>
2020-08-07 17:41:46 -0700

    try
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/nlp/data/__init__.py
+++ nemo/collections/nlp/data/__init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nemo.collections.nlp.data.data_utils import *
 from nemo.collections.nlp.data.language_modeling.lm_bert_dataset import (
     BertPretrainingDataset,
     BertPretrainingPreprocessedDataloader,
