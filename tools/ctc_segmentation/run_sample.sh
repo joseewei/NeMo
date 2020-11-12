5a6a8638641f95325aeee9d46afb6ec3558f8bbd
ekmb <ebakhturina@nvidia.com>
2020-11-12 13:16:53 -0800

    clean up
    Signed-off-by: ekmb <ebakhturina@nvidia.com>

--- tools/ctc_segmentation/run_sample.sh
+++ tools/ctc_segmentation/run_sample.sh
@@ -23,9 +23,9 @@ echo "DATA_DIR = $DATA_DIR"
 echo "OUTPUT_DIR = $OUTPUT_DIR"
 echo "MIN_SCORE = $MIN_SCORE"
 echo "CUT_PREFIX = $CUT_PREFIX"
-echo "SCRIPTS_DIR= $SCRIPTS_DIR"
-echo "OFFSET= $OFFSET"
-echo "LANGUAGE= $LANGUAGE"
+echo "SCRIPTS_DIR = $SCRIPTS_DIR"
+echo "OFFSET = $OFFSET"
+echo "LANGUAGE = $LANGUAGE"
 
 if [[ -z $MODEL_NAME_OR_PATH ]] || [[ -z $DATA_DIR ]] || [[ -z $OUTPUT_DIR ]]; then
   echo "Usage: $(basename "$0")
@@ -51,8 +51,9 @@ python $SCRIPTS_DIR/prepare_data.py \
 
 # STEP #2
 # Run CTC-segmenatation
-# one might wants to perform alignemnt with various window sizes
-# note if the alignment with the initial window size won't be found, the window size will be double to re-attempt
+# one might want to perform alignment with various window sizes
+# note if the alignment with the initial window size isn't found, the window size will be double to re-attempt
+# alignment
 for WINDOW in 8000 12000
 do
   python $SCRIPTS_DIR/run_ctc_segmentation.py \
@@ -70,10 +71,10 @@ python $SCRIPTS_DIR/verify_segments.py \
 # STEP #4
 # Cut the original audio files based on the alignments
 # (use --alignment=$OUTPUT_DIR/segments if only 1 WINDOW size was used in the Step #2)
-# 3 manifests and corresponding clips folders will be created:
-# - high scored clips
-# - low scored clips
-# - deleted segments
+# Three manifests and corresponding clips folders will be created:
+#   - high scored clips
+#   - low scored clips
+#   - deleted segments
 python $SCRIPTS_DIR/cut_audio.py \
 --output_dir=$OUTPUT_DIR \
 --model=$MODEL_NAME_OR_PATH \
@@ -82,11 +83,11 @@ python $SCRIPTS_DIR/cut_audio.py \
 --offset=$OFFSET || exit
 
 # STEP #5 (Optional)
-# If multiple audio files were segmented in the step #2, this step will aggregate manifests for high scored segments
+# If multiple audio files were segmented in the step #2, this step will aggregate manifests with high scored segments
 # for all audio files into all_manifest.json
-# Also a separate manifest with samples from across all high scored segments will credated if --num_samples > 0
-# --num_samples samples will be taken from the beginning, end and the middle of the each audio file manifest and
-# will be stored at sample_manifest.json
+# Also a separate manifest with samples from across all high scored segments will be credated if --num_samples > 0
+# --num_samples samples will be taken from the beginning, end and the middle of the each audio file high score manifest
+# and will be stored at sample_manifest.json
 python $SCRIPTS_DIR/process_manifests.py \
 --output_dir=$OUTPUT_DIR \
 --manifests_dir=$OUTPUT_DIR/manifests/ \
