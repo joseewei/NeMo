773eddc92ba7eb00b872414bbe3937dbd478238a
MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
2020-11-22 19:36:04 -0800

    Shard suffix logic
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>

--- examples/nlp/machine_translation/data_scripts/sharded_backtranslation.sh
+++ examples/nlp/machine_translation/data_scripts/sharded_backtranslation.sh
@@ -1,4 +1,3 @@
-
 PYTHONUNBUFFERED=1
 NUM_SHARDS=16
 NUM_GPUS=15
@@ -8,8 +7,15 @@ model_path=$3
 
 split -n ${NUM_SHARDS} -d -a 2 ${mono_data} /tmp/shard
 
-for i in $(seq 0 $NUM_GPUS); do echo $i; done
+for i in $(seq 0 $NUM_GPUS); do
+    if [ $i < 9 ]
+        then
+        suffix=$i$i
+    else
+        suffix=$i
+    fi
     CUDA_VISIBLE_DEVICES=${i} python ../generate_noisy_backtranslation_data.py \
         --model ${model_path} \
-        --text2translate /tmp/shard${i}${i} \
+        --text2translate /tmp/shard${suffix} \
         --output ${output_path}/backtranslation.shard.${i} &
+done
