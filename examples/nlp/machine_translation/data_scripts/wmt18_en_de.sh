968c335a603a5ce7fdc970070e7514e73e09df3d
MaximumEntropy <sandeep.subramanian.1@umontreal.ca>
2020-11-23 00:04:37 -0800

    Add data processing for EnDe WMT18 with monolingual data for backtranslation
    Signed-off-by: MaximumEntropy <sandeep.subramanian.1@umontreal.ca>

---
+++ examples/nlp/machine_translation/data_scripts/wmt18_en_de.sh
@@ -0,0 +1,215 @@
+URLS=(
+    "http://statmt.org/wmt13/training-parallel-europarl-v7.tgz"
+    "http://statmt.org/wmt13/training-parallel-commoncrawl.tgz"
+    "http://data.statmt.org/wmt18/translation-task/training-parallel-nc-v13.tgz"
+    "http://data.statmt.org/wmt18/translation-task/rapid2016.tgz"
+    "http://data.statmt.org/wmt17/translation-task/dev.tgz"
+    "http://statmt.org/wmt14/test-full.tgz"
+)
+FILES=(
+    "training-parallel-europarl-v7.tgz"
+    "training-parallel-commoncrawl.tgz"
+    "training-parallel-nc-v13.tgz"
+    "rapid2016.tgz"
+    "dev.tgz"
+    "test-full.tgz"
+)
+CORPORA=(
+    "training/europarl-v7.de-en"
+    "commoncrawl.de-en"
+    "training-parallel-nc-v13/news-commentary-v13.de-en"
+    "rapid2016.de-en"
+)
+
+URLS_mono_de=(
+    "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2007.de.shuffled.gz"
+    "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2008.de.shuffled.gz"
+    "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2009.de.shuffled.gz"
+    "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2010.de.shuffled.gz"
+    "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2011.de.shuffled.gz"
+    "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2012.de.shuffled.gz"
+    "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2013.de.shuffled.gz"
+    "http://www.statmt.org/wmt15/training-monolingual-news-crawl-v2/news.2014.de.shuffled.v2.gz"
+    "http://data.statmt.org/wmt16/translation-task/news.2015.de.shuffled.gz"
+    "http://data.statmt.org/wmt17/translation-task/news.2016.de.shuffled.gz"
+    "http://data.statmt.org/wmt18/translation-task/news.2017.de.shuffled.deduped.gz"
+)
+
+URLS_mono_en=(
+    "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2007.en.shuffled.gz"
+    "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2008.en.shuffled.gz"
+    "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2009.en.shuffled.gz"
+    "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2010.en.shuffled.gz"
+    "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2011.en.shuffled.gz"
+    "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2012.en.shuffled.gz"
+    "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2013.en.shuffled.gz"
+    "http://www.statmt.org/wmt15/training-monolingual-news-crawl-v2/news.2014.en.shuffled.v2.gz"
+    "http://data.statmt.org/wmt16/translation-task/news.2015.en.shuffled.gz"
+    "http://data.statmt.org/wmt17/translation-task/news.2016.en.shuffled.gz"
+    "http://data.statmt.org/wmt18/translation-task/news.2017.en.shuffled.deduped.gz"
+)
+
+FILES_de=(
+    "news.2007.de.shuffled.gz"
+    "news.2008.de.shuffled.gz"
+    "news.2009.de.shuffled.gz"
+    "news.2010.de.shuffled.gz"
+    "news.2011.de.shuffled.gz"
+    "news.2012.de.shuffled.gz"
+    "news.2013.de.shuffled.gz"
+    "news.2014.de.shuffled.v2.gz"
+    "news.2015.de.shuffled.gz"
+    "news.2016.de.shuffled.gz"
+    "news.2017.de.shuffled.deduped.gz"
+)
+
+FILES_en=(
+    "news.2007.en.shuffled.gz"
+    "news.2008.en.shuffled.gz"
+    "news.2009.en.shuffled.gz"
+    "news.2010.en.shuffled.gz"
+    "news.2011.en.shuffled.gz"
+    "news.2012.en.shuffled.gz"
+    "news.2013.en.shuffled.gz"
+    "news.2014.en.shuffled.v2.gz"
+    "news.2015.en.shuffled.gz"
+    "news.2016.en.shuffled.gz"
+    "news.2017.en.shuffled.deduped.gz"
+)
+
+OUTDIR=wmt18_en_de
+lang1=en
+lang2=de
+lang=en-de
+rev_lang=de-en
+orig=orig
+
+mkdir -p $OUTDIR
+
+cd $orig
+
+for ((i=0;i<${#URLS[@]};++i)); do
+    file=${FILES[i]}
+    if [ -f $file ]; then
+        echo "$file already exists, skipping download"
+    else
+        url=${URLS[i]}
+        wget "$url"
+        if [ -f $file ]; then
+            echo "$url successfully downloaded."
+        else
+            echo "$url not successfully downloaded."
+            exit 1
+        fi
+        if [ ${file: -4} == ".tgz" ]; then
+            tar zxvf $file
+        elif [ ${file: -4} == ".tar" ]; then
+            tar xvf $file
+        fi
+    fi
+done
+
+cd ..
+
+echo "pre-processing train data..."
+for l in $lang1 $lang2; do
+    for f in "${CORPORA[@]}"; do
+        cat $orig/$f.$l >> $OUTDIR/train.$lang.$l
+    done
+done
+
+if [ ! -f clean-corpus-n.perl ]
+then
+    wget https://raw.githubusercontent.com/moses-smt/mosesdecoder/master/scripts/training/clean-corpus-n.perl
+    chmod +x clean-corpus-n.perl
+fi
+
+./clean-corpus-n.perl -ratio 1.5 ${OUTDIR}/train.$lang $lang1 $lang2 ${OUTDIR}/train.clean 1 250
+
+echo "Fetching Validation data $lang" 
+sacrebleu -t wmt13 -l $lang --echo src > ${OUTDIR}/wmt13-$lang.src
+sacrebleu -t wmt13 -l $lang --echo ref > ${OUTDIR}/wmt13-$lang.ref
+
+echo "Fetching Test data $lang" 
+sacrebleu -t wmt14 -l $lang --echo src > ${OUTDIR}/wmt14-$lang.src
+sacrebleu -t wmt14 -l $lang --echo ref > ${OUTDIR}/wmt14-$lang.ref
+
+echo "Fetching Validation data $rev_lang" 
+sacrebleu -t wmt13 -l $rev_lang --echo src > ${OUTDIR}/wmt13-$rev_lang.src
+sacrebleu -t wmt13 -l $rev_lang --echo ref > ${OUTDIR}/wmt13-$rev_lang.ref
+
+echo "Fetching Test data $rev_lang" 
+sacrebleu -t wmt14 -l $rev_lang --echo src > ${OUTDIR}/wmt14-$rev_lang.src
+sacrebleu -t wmt14 -l $rev_lang --echo ref > ${OUTDIR}/wmt14-$rev_lang.ref
+
+SUBSAMPLE_SIZE=25000000
+
+OUTDIR_en=wmt18_en_mono
+mkdir -p $OUTDIR_en
+
+cd $orig
+
+echo "Done Processing Parallel Corpus, Fetching Monolingual Data ..."
+
+echo "Fetching English Monolingual data ..."
+
+for ((i=0;i<${#URLS_mono_en[@]};++i)); do
+    file=${FILES_en[i]}
+    if [ -f $file ]; then
+        echo "$file already exists, skipping download"
+    else
+        url=${URLS_mono_en[i]}
+        wget "$url"
+    fi
+done
+
+cd ..
+
+echo "Subsampling data ..."
+if [ -f ${OUTDIR_en}/monolingual.${SUBSAMPLE_SIZE}.en ]; then
+    echo "found monolingual sample, skipping shuffle/sample/tokenize"
+else
+    gzip -c -d -k $(for FILE in "${FILES_en[@]}"; do echo $orig/$FILE; done) \
+    | shuf -n $SUBSAMPLE_SIZE > $OUTDIR_en/monolingual.${SUBSAMPLE_SIZE}.en
+fi
+
+echo "Deduplicating data ..."
+if [ -f ${OUTDIR_en}/monolingual.dedup.${SUBSAMPLE_SIZE}.en ]; then
+    echo "found deduplicated monolingual sample, skipping deduplication step"
+else
+    awk '!a[$0]++' ${OUTDIR_en}/monolingual.${SUBSAMPLE_SIZE}.en > ${OUTDIR_en}/bpe.monolingual.dedup.${SUBSAMPLE_SIZE}.en
+fi
+
+echo "Fetching German Monolingual data ..."
+
+OUTDIR_de=wmt18_de_mono
+mkdir -p $OUTDIR_de
+
+cd $orig
+
+for ((i=0;i<${#URLS_mono_de[@]};++i)); do
+    file=${FILES_de[i]}
+    if [ -f $file ]; then
+        echo "$file already exists, skipping download"
+    else
+        url=${URLS_mono_de[i]}
+        wget "$url"
+    fi
+done
+
+cd ..
+
+echo "Subsampling data ..."
+if [ -f ${OUTDIR_de}/monolingual.${SUBSAMPLE_SIZE}.de ]; then
+    echo "found monolingual sample, skipping shuffle/sample/tokenize"
+else
+    gzip -c -d -k $(for FILE in "${FILES_de[@]}"; do echo $orig/$FILE; done) \
+    | shuf -n $SUBSAMPLE_SIZE > ${OUTDIR_de}/monolingual.${SUBSAMPLE_SIZE}.de
+fi
+
+echo "Deduplicating data ..."
+if [ -f ${OUTDIR_de}/monolingual.dedup.${SUBSAMPLE_SIZE}.de ]; then
+    echo "found deduplicated monolingual sample, skipping deduplication step"
+else
+    awk '!a[$0]++' ${OUTDIR_de}/monolingual.${SUBSAMPLE_SIZE}.de > ${OUTDIR_de}/bpe.monolingual.dedup.${SUBSAMPLE_SIZE}.de
+fi
