2891a5d8356bc7599627f58f9d8cdc8f707badbf
ericharper <complex451@gmail.com>
2021-05-18 14:50:49 -0600

    updating nlp overrides for mp nmt
    Signed-off-by: ericharper <complex451@gmail.com>

--- examples/nlp/machine_translation/enc_dec_nmt.py
+++ examples/nlp/machine_translation/enc_dec_nmt.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from dataclasses import dataclass
+from nemo.collections.nlp.parts.nlp_overrides import NLPDDPPlugin
 from typing import Optional
 
 from pytorch_lightning import Trainer
@@ -107,7 +108,7 @@ def main(cfg: MTEncDecConfig) -> None:
     logging.info(f'Config: {cfg.pretty()}')
 
     # training is managed by PyTorch Lightning
-    trainer = Trainer(**cfg.trainer)
+    trainer = Trainer(plugins=[NLPDDPPlugin()], **cfg.trainer)
 
     # tokenizers will be trained and and tarred training data will be created if needed
     # model config is then updated
