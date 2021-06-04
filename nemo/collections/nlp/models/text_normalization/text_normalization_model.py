068aae658731c436772a62459937638ea4ae9edc
Yang Zhang <yangzhang@nvidia.com>
2021-06-03 17:32:39 -0700

    added inference, allows bs>1, no metric computation yet, empty tag instances not handled yet, no inference dataset yet
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- nemo/collections/nlp/models/text_normalization/text_normalization_model.py
+++ nemo/collections/nlp/models/text_normalization/text_normalization_model.py
@@ -16,6 +16,7 @@ import json
 import os
 from typing import Dict, Optional
 
+import numpy as np
 import torch
 from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning import Trainer
@@ -24,6 +25,7 @@ from torch.utils.data import DataLoader
 
 from nemo.collections.common.losses import CrossEntropyLoss
 from nemo.collections.nlp.data import TextNormalizationDataset
+from nemo.collections.nlp.data.text_normalization.text_normalization_dataset import tag_labels
 from nemo.collections.nlp.metrics.classification_report import ClassificationReport
 from nemo.collections.nlp.models.nlp_model import NLPModel
 from nemo.collections.nlp.models.text_normalization.modules import (
@@ -115,6 +117,20 @@ class TextNormalizationModel(NLPModel):
             num_classes=cfg.tagger.num_classes, mode='micro', dist_sync_on_step=True
         )
 
+    def tagger_forward(self, context_output):
+        context_to_tagger = self.context_project(context_output[:, :, self._cfg.context.hidden_size :])
+        context_to_tagger = nn.functional.tanh(context_to_tagger)
+        tagger_output, _ = self.tagger_decoder(context_to_tagger)
+        tagger_logits = self.tagger_output_project(tagger_output)
+        return tagger_logits
+
+    def get_context(self, context_ids, len_context):
+        context_embedding = self.context_embedding(context_ids)
+        context_output, _ = self.context_encoder(input_seqs=context_embedding, input_lengths=len_context)
+        left_context = context_output[:, :, : self._cfg.context.hidden_size].view(-1, self._cfg.context.hidden_size)
+        right_context = context_output[:, :, self._cfg.context.hidden_size :].view(-1, self._cfg.context.hidden_size)
+        return context_output, left_context, right_context
+
     # @typecheck()
     def forward(
         self,
@@ -126,32 +142,21 @@ class TextNormalizationModel(NLPModel):
         output_ids,
         l_context_ids,
         r_context_ids,
-        max_len: Optional[int] = None
+        max_len: Optional[int] = None,
     ):
-        batch_size = len(context_ids)
-        context_embedding = self.context_embedding(context_ids)
-        context_output, _ = self.context_encoder(input_seqs=context_embedding, input_lengths=len_context)
+        context_output, left_context, right_context = self.get_context(context_ids, len_context)
 
-        context_to_tagger = self.context_project(context_output[:, :, self._cfg.context.hidden_size :])
-        context_to_tagger = nn.functional.tanh(context_to_tagger)
-        tagger_output, _ = self.tagger_decoder(context_to_tagger)
-        tagger_logits = self.tagger_output_project(tagger_output)
-
-        left_context = context_output[:, :, : self._cfg.context.hidden_size].view(-1, self._cfg.context.hidden_size)
-        right_context = context_output[:, :, self._cfg.context.hidden_size :].view(-1, self._cfg.context.hidden_size)
+        batch_size, max_context_length = len(context_ids)
         context_lr = torch.cat(
             [
-                left_context[torch.arange(batch_size).to(self._device) * batch_size + l_context_ids],
-                right_context[torch.arange(batch_size).to(self._device) * batch_size + r_context_ids],
+                left_context[torch.arange(batch_size).to(self._device) * max_context_length + l_context_ids],
+                right_context[torch.arange(batch_size).to(self._device) * max_context_length + r_context_ids],
             ],
             dim=-1,
         ).unsqueeze(0)
+        tagger_logits = self.tagger_forward(context_output)
         seq_logits = self.seq2seq(
-            src=input_ids,
-            trg=output_ids,
-            src_lengths=len_input,
-            decoder_init_hidden=context_lr,
-            max_len=max_len
+            src=input_ids, trg=output_ids, src_lengths=len_input, decoder_init_hidden=context_lr, max_len=max_len
         )
 
         return tagger_logits, seq_logits
@@ -200,14 +205,7 @@ class TextNormalizationModel(NLPModel):
         bs, max_context_length = context_ids.shape
         _, max_target_length = output_ids.shape
         tagger_logits, seq_logits = self.forward(
-            context_ids,
-            tag_ids,
-            len_context,
-            input_ids,
-            len_input,
-            output_ids,
-            l_context_ids,
-            r_context_ids,
+            context_ids, tag_ids, len_context, input_ids, len_input, output_ids, l_context_ids, r_context_ids,
         )
         tagger_loss_mask = torch.arange(max_context_length).to(self._device).expand(
             bs, max_context_length
@@ -244,14 +242,7 @@ class TextNormalizationModel(NLPModel):
         bs, max_context_length = context_ids.shape
         _, max_target_length = output_ids.shape
         tagger_logits, seq_logits = self.forward(
-            context_ids,
-            tag_ids,
-            len_context,
-            input_ids,
-            len_input,
-            output_ids,
-            l_context_ids,
-            r_context_ids,
+            context_ids, tag_ids, len_context, input_ids, len_input, output_ids, l_context_ids, r_context_ids,
         )
         tagger_loss_mask = torch.arange(max_context_length).to(self._device).expand(
             bs, max_context_length
@@ -292,6 +283,136 @@ class TextNormalizationModel(NLPModel):
 
     def test_step(self, batch, batch_idx):
         return self.validation_step(batch, batch_idx)
+
+    def test_epoch_end(self, outputs):
+        """
+        Called at test time to aggregate outputs.
+        outputs: list of individual outputs of each validation step.
+        """
+        avg_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
+
+        # calculate metrics and classification report
+        precision, recall, f1, report = self.classification_report.compute()
+
+        logging.info(report)
+
+        self.log('test_loss', avg_loss, prog_bar=True)
+        self.log('precision', precision)
+        self.log('f1', f1)
+        self.log('recall', recall)
+
+        self.classification_report.reset()
+
+    @torch.no_grad()
+    def infer(self, cfg: DictConfig, max_target_length=10):
+        # store predictions for all queries in a single list
+        all_preds = []
+        mode = self.training
+        try:
+            device = 'cuda' if torch.cuda.is_available() else 'cpu'
+            # Switch model to evaluation modef
+            self.eval()
+            self.to(device)
+            infer_datalayer = self._setup_dataloader_from_config(cfg=cfg)
+
+            for batch in infer_datalayer:
+                (
+                    context_ids,
+                    tag_ids,
+                    len_context,
+                    input_ids,
+                    len_input,
+                    output_ids,
+                    len_output,
+                    l_context_ids,
+                    r_context_ids,
+                ) = batch
+                context_ids = context_ids.to(device)
+                len_context = len_context.to(device)
+                bs, max_context_length = context_ids.shape
+
+                # get context encoding, and left and right context embeddings
+                context_output, left_context, right_context = self.get_context(context_ids, len_context)
+
+                # get tagger logits
+                tagger_logits = self.tagger_forward(context_output)
+
+                tagger_loss_mask = torch.arange(max_context_length).to(device).expand(
+                    bs, max_context_length
+                ) < len_context.to(device).unsqueeze(1)
+                tagger_loss_mask[:, 0] = False  # do not allow first token since this should correspond to bos
+                tagger_loss_mask[:, -1] = False  # do not allow last token since this should correspond to eos
+                tag_preds = torch.argmax(tagger_logits, axis=-1)
+                semiotic_tag_preds = torch.logical_or(tag_preds == tag_labels['B-I'], tag_preds == tag_labels['B-M'])
+                semiotic_tag_preds.masked_fill_(
+                    tagger_loss_mask == False, False
+                )  # do not consider preds outside of mask
+
+                l_context_ids = torch.where(
+                    torch.logical_and(semiotic_tag_preds[:, :-1] == False, semiotic_tag_preds[:, 1:] == True)
+                )
+                r_context_ids = torch.where(
+                    torch.logical_and(semiotic_tag_preds[:, 1:] == False, semiotic_tag_preds[:, :-1] == True)
+                )
+                r_context_ids = (r_context_ids[0], r_context_ids[1] + 1)
+
+                assert (
+                    torch.all(l_context_ids[1] < r_context_ids[1]),
+                    "left context start needs to be smaller than right context end",
+                )
+
+                context_lr = torch.cat(
+                    [
+                        left_context[l_context_ids[0] * max_context_length + l_context_ids[1]],
+                        right_context[r_context_ids[0] * max_context_length + r_context_ids[1]],
+                    ],
+                    dim=-1,
+                ).unsqueeze(0)
+
+                input_ids = []
+                for i in range(len(l_context_ids[0])):
+                    batch_id = l_context_ids[0][i]
+                    input_str = self._tokenizer_context.ids_to_text(
+                        tensor2list(context_ids[batch_id][l_context_ids[1][i] + 1 : r_context_ids[1][i]])
+                    )
+                    input_ids.append([self._tokenizer_encoder.bos_id] + self._tokenizer_encoder.text_to_ids(input_str))
+
+                len_input = [len(x) for x in input_ids]
+                max_input_length = max(len_input)
+                len_input = torch.LongTensor(len_input).to(device)
+                for i in range(len(input_ids)):
+                    pad_width = max_input_length - len(input_ids[i])
+                    input_ids[i] = np.pad(
+                        input_ids[i], pad_width=[0, pad_width], constant_values=self._tokenizer_encoder.pad_id
+                    )
+
+                input_ids = torch.LongTensor(input_ids).to(device)
+                decoder_start = (
+                    torch.tensor([self._tokenizer_decoder.bos_id]).repeat(input_ids.shape[0]).unsqueeze(1).to(device)
+                )
+
+                seq_logits = self.seq2seq(
+                    src=input_ids,
+                    trg=decoder_start,
+                    src_lengths=len_input,
+                    decoder_init_hidden=context_lr,
+                    max_len=max_target_length,
+                )
+
+                seq_preds = tensor2list(torch.argmax(seq_logits, axis=-1))
+                for i in range(len(seq_preds)):
+                    if self._tokenizer_decoder.eos_id in seq_preds[i]:
+                        seq_preds[i] = seq_preds[i][: seq_preds[i].index(self._tokenizer_decoder.eos_id)]
+                for input, pred in zip(tensor2list(input_ids), seq_preds):
+                    print('raw :', self._tokenizer_encoder.ids_to_text(input))
+                    pred = self._tokenizer_decoder.ids_to_text(pred)
+                    print('norm:', pred)
+                    all_preds.append(pred)
+
+        finally:
+            # set mode back to its original value
+            self.train(mode=mode)
+        return all_preds
 
     def setup_training_data(self, train_data_config: Optional[DictConfig]):
         if not train_data_config or not train_data_config.file:
