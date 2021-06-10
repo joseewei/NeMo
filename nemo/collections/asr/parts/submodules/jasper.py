dcf650f26df94921f4c8e4bf50d025b395f131c3
nithinraok <nithinrao.koluguri@gmail.com>
2021-06-10 16:32:45 -0700

    res2net_work2
    Signed-off-by: nithinraok <nithinrao.koluguri@gmail.com>

--- nemo/collections/asr/parts/submodules/jasper.py
+++ nemo/collections/asr/parts/submodules/jasper.py
@@ -279,6 +279,7 @@ class SERes2NetBlock(nn.Module):
         self.res2net_block = Res2NetBlock(
             out_channels, out_channels, res2net_scale, dilation
         )
+        # self.res2net_block = nn.Conv1d(out_channels,out_channels,kernel_size=3,dilation=1,padding=1,groups=res2net_scale)
         self.tdnn2 = TDNN_Block(
             out_channels,
             out_channels,
